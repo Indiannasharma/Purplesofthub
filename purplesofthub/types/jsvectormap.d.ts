6db41fc57611e3ccf1a9b245edf5c8b7c6119d75
PurpleSoftHub Dev <dev@purplesofthub.com>
2026-04-06 23:04:58 +0100

    chore: reorganize project structure - move files to root level

--- purplesofthub/types/jsvectormap.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'jsvectormap' {
-  export default class jsvectormap {
-    constructor(options: any);
-    destroy(): void;
-  }
-}
-
-declare module 'jsvectormap/dist/maps/world.js';
